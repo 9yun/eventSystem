c035546dff72e13d5f381482f22e2123b16071cf
Adithya93 <ra102@duke.edu>
2017-02-22 12:43:22 -0500

    Complete basic config and DB setup, about to create User model and simple authentication

--- eventSystem/views.py
+++ eventSystem/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Greetings! This will soon be an events RSVP app :)")
--- events/settings.py
+++ events/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['10.236.48.19']
 # Application definition
 
 INSTALLED_APPS = [
+    'eventSystem.apps.EventsystemConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,9 @@ WSGI_APPLICATION = 'events.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eventSystem',
     }
 }
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
