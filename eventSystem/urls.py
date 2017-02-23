cac54a73716234a3dc6a7aedb4236edd3bb04cd1
Adithya93 <ra102@duke.edu>
2017-02-23 00:50:48 -0500

    Simple templates for user home page and event home page, ensure correct flow from login to user to event pages

--- eventSystem/urls.py
+++ eventSystem/urls.py
@@ -5,10 +5,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    #url(r'^login/$', views.login, name='login'),
-    #url('^', include('django.contrib.auth.urls')),
-    #url(r'^login/$', auth_views.login),
     url(r'^login/$', views.user_login, name='user_login'),
     url(r'^register/$', views.user_reg, name='user_reg'),
-    url(r'^users/(?P<username>[a-zA-Z0-9_]+)/$', views.user_home, name='user_home')
+    url(r'^users/(?P<username>[a-zA-Z0-9_]+)/$', views.user_home, name='user_home'),
+    url(r'^events/(?P<eventname>[a-zA-Z0-9_]+)/$', views.event_home, name='event_home'),
 ]
