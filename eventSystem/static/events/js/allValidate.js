9b391d80458f86745fbbf8604294ff2befeb1a5f
ikazubes22 <ian.kaszubski@hotmail.com>
2017-03-01 18:05:53 -0500

    Events may no longer have special character in names

--- eventSystem/static/events/js/allValidate.js
+++ eventSystem/static/events/js/allValidate.js
@@ -1,3 +1,26 @@
+function allValidate() {
+    if (!validateTime()) {
+        return false;
+    }
+    if (!validateEventName()) {
+        return false;
+    }
+    return true;
+}
+
+function validateEventName() {
+    var x = document.getElementsByName("eventname");
+    x = x[0];
+    re = /[^a-zA-Z0-9_ ]/g;
+    if(x.value.match(re)) {
+      alert("Event name may only contain letters, numbers, underscores and spaces.");
+      x.focus();
+      x.style="color:red";
+      return false;
+    }
+    return true;
+}
+
 function validateTime() {
   var x = document.getElementsByName("start_time");
   x = x[0];
