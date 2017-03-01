7e0e0e0552f8592ddd61e85d3a4a9ccde9f4771b
ikazubes22 <ian.kaszubski@hotmail.com>
2017-03-01 17:56:59 -0500

    validateTime js now fixes spacing issues before sending them off to the server

--- eventSystem/static/events/js/validateTime.js
+++ eventSystem/static/events/js/validateTime.js
@@ -1,21 +1,28 @@
 function validateTime() {
-    var x = document.forms["input_form"]["starttime"];
-    re = /^\d{1,2}:\d{2}\s*([ap]m)?$/i;
-    if(x.value != '' && !x.value.match(re)) {
-      alert("Invalid time format: " + x.value);
-      x.focus();
-      document.forms["input_form"]["starttime"].value = "HH:MM AM/PM";
-      document.forms["input_form"]["starttime"].style="color:red";
-      return false;
-    }
-    x = document.forms["input_form"]["endtime"];
-    re = /^\d{1,2}:\d{2}\s*([ap]m)?$/i;
-    if(x.value != '' && !x.value.match(re)) {
-      alert("Invalid time format: " + x.value);
-      x.focus();
-      document.forms["input_form"]["endtime"].value = "HH:MM AM/PM";
-      document.forms["input_form"]["endtime"].style="color:red";
-      return false;
-    }
-    return true;
+  var x = document.getElementsByName("start_time");
+  x = x[0];
+  re = /^(\d{1,2}):(\d{2})\s*([ap]m)/i;
+  if (x.value != '' && !x.value.match(re)) {
+    alert("Invalid time format: " + x.value);
+    x.focus();
+    x.value = "HH:MM AM/PM";
+    x.style = "color:red";
+    return false;
+  }
+  // Modify it for the server
+  var matches = re.exec(x.value);
+  x.value = matches[1] + ":" + matches[2] + " " + matches[3];
+  x = document.getElementsByName("end_time");
+  x = x[0];
+  re = /^(\d{1,2}):(\d{2})\s*([ap]m)/i;
+  if (x.value != '' && !x.value.match(re)) {
+    alert("Invalid time format: " + x.value);
+    x.focus();
+    x.value = "HH:MM AM/PM";
+    x.style = "color:red";
+    return false;
+  }
+  matches = re.exec(x.value);
+  x.value = matches[1] + ":" + matches[2] + " " + matches[3];
+  return true;
 }
